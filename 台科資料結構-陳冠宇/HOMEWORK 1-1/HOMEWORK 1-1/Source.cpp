1259cb57f3e2d3e6d0d52e182550197e5a288666
郝 廖 <frakwu@gmail.com>
2020-10-09 01:51:48 +0800

    1-1 完成，內涵linked list版本與array 版本

--- 台科資料結構-陳冠宇/HOMEWORK 1-1/HOMEWORK 1-1/Source.cpp
+++ 台科資料結構-陳冠宇/HOMEWORK 1-1/HOMEWORK 1-1/Source.cpp
@@ -1,3 +1,78 @@
+//linked list版本，效率更高
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+	struct node* next;
+	struct node* prev;
+	int value;
+} Node;
+
+Node* create_node(int value, Node* prev)
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->next = NULL;
+	node->value = value;
+	node->prev = prev;
+	return node;
+}
+int main() {
+	int N, M, first;
+	Node* head, * now;
+	int size = 0, index = 0;
+	scanf("%d%d%d", &N, &M, &first);
+	Node** backup = (Node**)malloc(N * sizeof(Node*));
+	head = create_node(first, NULL);
+	now = head;
+	backup[0] = head;
+	size = N;
+	for (int i = 1;i < N;i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		now->next = create_node(tmp, now);
+		now = now->next;
+		backup[i] = now;
+	}
+	now = head;
+	while ((index + M) <= size) {
+		Node* scan = now->next;
+		int i, prev = now->value;
+		for (i = 1;i < M;i++) {
+			if (prev >= scan->value) break;
+			prev = scan->value;
+			scan = scan->next;
+		}
+		if (i == M) {
+			if (now == head) {
+				head = now = scan;
+				head->prev = NULL;
+				index = 0;
+			}
+			else {
+				now->prev->next = scan;
+				if (scan) scan->prev = now->prev;
+				index -= (M - 1);
+				if (index < 0) index = 0;
+				for (int i = 1;i < M && now->prev;i++) now = now->prev;
+			}
+			size -= M;
+		}
+		else {
+			index++;
+			now = now->next;
+		}
+	}
+	while (head) {
+		printf("%d ", head->value);
+		head = head->next;
+	}
+	for (int i = 0;i < N;++i)	free(backup[i]);
+	free(backup);
+	return 0;
+}
+
+//c++ array版本，效率低
 /*#include <iostream>
 using namespace std;
 template <class T>
@@ -60,80 +135,3 @@ int main() {
 
 	return 0;
 }*/
-
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct node {
-	struct node* next;
-	struct node* prev;
-	int value;
-} Node;
-
-typedef struct list {
-	Node* head;
-} List;
-
-Node* create_node(int value,Node* prev)
-{
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->next = NULL;
-	node->value = value;
-	node->prev = prev;
-	return node;
-}
-int main() {
-	int N, M,first;
-	List list;
-	Node* now;
-	int size = 0,index = 0;
-	scanf("%d%d%d", &N, &M,&first);
-	list.head = create_node(first,NULL);
-	now = list.head;
-	size = N;
-	for (int i = 1;i < N;i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		now->next = create_node(tmp,now);
-		now = now->next;
-	}
-	now = list.head;
-	while ((index + M) <= size && M!=1) {
-		Node* scan = now->next;
-		int i,prev = now->value;
-		for (i = 1;i < M;i++) {
-			if (prev >= scan->value) break;
-			prev = scan->value;
-			scan = scan->next;
-		}
-		if (i == M) {
-			if (now == list.head) {
-				list.head = scan;
-				now = scan;
-				list.head->prev = NULL;
-			}
-			else {
-				now = now->prev;
-				now->next = scan;
-				if(scan) scan->prev = now;
-				index -= (M - 1);
-				index = index < 0 ? 0 : index;
-				for (int i = 0;i < (M - 1);i++) {
-					now = now->prev ? now->prev : now;
-				}
-			}
-			size -= M;
-		}
-		else {
-			index++;
-			now = now->next;
-		}
-	}
-	now = list.head;
-	while(now) {
-		printf("%d ", now->value);
-		now = now->next;
-	}
-	return 0;
-}
