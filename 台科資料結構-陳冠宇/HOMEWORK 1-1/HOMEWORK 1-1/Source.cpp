83b4ea1115792afa61c6804924fdc167777cfde0
郝 廖 <frakwu@gmail.com>
2020-10-08 18:19:18 +0800

    10/8

--- 台科資料結構-陳冠宇/HOMEWORK 1-1/HOMEWORK 1-1/Source.cpp
+++ 台科資料結構-陳冠宇/HOMEWORK 1-1/HOMEWORK 1-1/Source.cpp
@@ -1,49 +1,46 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 template <class T>
 class vec {
 public:
 	T& operator[](int index) {
-		return arr ? arr1[index] : arr0[index];
+		return arr[index];
 	}
 	vec(int isize) {
-		arr0 = new T[isize];
-		arr1 = new T[isize];
+		arr = new T[isize];
 		size = isize;
 		capacity = isize;
+		head = arr;
 	}
 	~vec() {
-		delete[] arr0;
-		delete[] arr1;
+		delete[] head;
 	}
-	void erase(int index, int num) {
-		T* src, * dst;
-		src = arr ? arr1 : arr0;
-		dst = arr ? arr0 : arr1;
-		for (int srci = 0, dsti = 0;srci < size;srci++, dsti++) {
-			if (srci == index) {
-				srci += num;
+	void erase(int index, int count) {
+		if (index+index+count < size) {
+			for (int i = index - 1;i >= 0;--i) {
+				arr[i + count] = arr[i];
 			}
-			dst[dsti] = src[srci];
+			arr += count;
 		}
-		arr = !arr;
-		size -= num;
+		else {
+			for (int i = index + count;i < size;++i) {
+				arr[i - count] = arr[i];
+			}
+		}
+		size -= count;
 	}
-	//private:
-	T* arr0 = nullptr;
-	T* arr1 = nullptr;
-	bool arr = false;//arr0=>false arr1=>true
+	T* arr = nullptr;
+	T* head = nullptr;
 	int size = 0;
 	int capacity = 0;
 };
 int main() {
-	int N, M;
+	int N, M, index = 0;
 	cin >> N >> M;
 	vec<int> sequences(N);
 	for (int i = 0;i < N;i++) {
-		cin >> sequences.arr0[i];
+		cin >> sequences.arr[i];
 	}
-	int index = 0;
 	while ((index + M) <= sequences.size) {
 		int i;
 		for (i = 1;i < M;i++) {
@@ -51,7 +48,8 @@ int main() {
 		}
 		if (i == M) {
 			sequences.erase(index, M);
-			index = 0;
+			index -= (M-1);
+			index = index < 0 ? 0 : index;
 		}
 		else index++;
 	}
@@ -60,5 +58,82 @@ int main() {
 	}
 	cout << endl;
 
+	return 0;
+}*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+	struct node* next;
+	struct node* prev;
+	int value;
+} Node;
+
+typedef struct list {
+	Node* head;
+} List;
+
+Node* create_node(int value,Node* prev)
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->next = NULL;
+	node->value = value;
+	node->prev = prev;
+	return node;
+}
+int main() {
+	int N, M,first;
+	List list;
+	Node* now;
+	int size = 0,index = 0;
+	scanf("%d%d%d", &N, &M,&first);
+	list.head = create_node(first,NULL);
+	now = list.head;
+	size = N;
+	for (int i = 1;i < N;i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		now->next = create_node(tmp,now);
+		now = now->next;
+	}
+	now = list.head;
+	while ((index + M) <= size && M!=1) {
+		Node* scan = now->next;
+		int i,prev = now->value;
+		for (i = 1;i < M;i++) {
+			if (prev >= scan->value) break;
+			prev = scan->value;
+			scan = scan->next;
+		}
+		if (i == M) {
+			if (now == list.head) {
+				list.head = scan;
+				now = scan;
+				list.head->prev = NULL;
+			}
+			else {
+				now = now->prev;
+				now->next = scan;
+				if(scan) scan->prev = now;
+				index -= (M - 1);
+				index = index < 0 ? 0 : index;
+				for (int i = 0;i < (M - 1);i++) {
+					now = now->prev ? now->prev : now;
+				}
+			}
+			size -= M;
+		}
+		else {
+			index++;
+			now = now->next;
+		}
+	}
+	now = list.head;
+	while(now) {
+		printf("%d ", now->value);
+		now = now->next;
+	}
 	return 0;
 }
